3d19f82ae61f9c5047f2b6aa366f693a12a64ac1
claraciriaco <m.clara.ciriaco@gmail.com>
2021-03-02 13:05:53 -0300

    db

--- app/models/tables.py
+++ app/models/tables.py
@@ -9,3 +9,35 @@ class User(db.Model):
     name = db.Column(db.String)
     email = db.Column(db.String, unique=True)
 
+    def __init__(self, username, password, name, email):
+        self.username = username
+        self.password = password
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return "<User %r>" % self.username
+
+class Book(db.Model):
+    __tablename__ = "books"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    title = db.Column(db.String, unique=True)
+    autor = db.Column(db.String)
+    datainicio = db.Column(db.String)
+    datafim = db.Column(db.String)
+    obs = db.Column(db.Text)
+
+    user = db.relationship('User', foreign_keys=user_id)
+
+    def __init__(self, title, autor, datainicio, datafim, obs, user_id):
+        self.title = title
+        self.autor = autor
+        self.datainicio = datainicio
+        self.datafim = datafim
+        self.obs = obs
+        self.user_id = user_id
+
+    def __repr(self):
+        return "<Book %r>" % self.title
