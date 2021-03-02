ad086a5ad5c591bd03f99f539e68bf4aa2d0b3b6
claraciriaco <m.clara.ciriaco@gmail.com>
2021-03-02 20:53:00 -0300

    db

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
+app.config.from_object('config')
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
